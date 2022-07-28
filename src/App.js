0f21209257bd8a3f194e84ad722e0f0779613193
infomagnum159 <infomagnum159@gmail.com>
2022-07-28 14:49:52 +0600

    Cleaned the code, added routers and imported the Provider, store, reducer, redux-thunk into the index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Layout from "./components/Layout/Layout";
+import Series from "./components/Series/Series";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Layout>
+        <Switch>
+            <Route path="/" exact component={Series}/>
+            <Route path="/shows/:id" exact component={Series}/>
+            <Route render={() => <h1>404 Not Found</h1>}/>
+        </Switch>
+    </Layout>
+);
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import {createStore, applyMiddleware, compose} from "redux";
+import {Provider} from "react-redux";
+import thunkMiddleware from 'redux-thunk'
+import reducer from "./store/reducer";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunkMiddleware))
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
