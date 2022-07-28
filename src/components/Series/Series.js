48ad15eb58b1f5f5aa75d994fa2165972666955c
infomagnum159 <infomagnum159@gmail.com>
2022-07-28 14:53:30 +0600

    Added a component for displaying information about the series

---
+++ src/components/Series/Series.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Series.css';
+
+
+const Series = props => {
+  return (
+    <div className="Series">
+        {props.children}
+    </div>
+  )
+};
+
+export default Series;
